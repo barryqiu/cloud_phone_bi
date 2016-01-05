52a10f9d6f79c87286cd91006f879c2beeb12084
barryqiu <qiuxiaobing90@126.com>
2016-01-05 22:04:19 +0800

    database

--- app/push/views.py
+++ app/push/views.py
@@ -35,12 +35,13 @@ def push_list(page):
     return render_template('push/list.html', pushes=pushes, pagination=pagination)
 
 
-def push_message():
+def push_message(message, message_type, platform, audience):
     _jpush = jpush.JPush(app.config['JPUSH_APP_KEY'], app.config['JPUSH_MASTER_SECRET'])
     push = _jpush.create_push()
-    push.message()
-    push.audience = jpush.all_
-    push.send()
+    push.message(msg_content=message, content_type=message_type)
+    push.audience = audience
+    push.platform = platform
+    push.send_validate()
 
 
 
