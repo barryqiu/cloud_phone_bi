f06267c3ec2d85943123e09eb4e3b2c1a8b2700a
barryqiu <qiuxiaobing90@126.com>
2016-02-23 12:22:42 +0800

    database

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     redis_store.init_app(app)
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint, url_prefix='/admin')
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
