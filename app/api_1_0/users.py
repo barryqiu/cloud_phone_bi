9ed8d359066c8c5485f56d19d7acc667813276d7
barryqiu <qiuxiaobing90@126.com>
2016-03-20 14:53:19 +0800

    database

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -69,30 +69,30 @@ def new_user():
 
 @api.route('/user/password', methods=['POST'])
 def edit_password():
-    try:
-        mobile_num = request.json.get('mobile_num')
-        if mobile_num is None or mobile_num == '':
-            raise ValidationError('user does not have a mobile_num')
-        password = request.json.get('password')
-        if password is None or password == '':
-            raise ValidationError('user does not have a password')
+    # try:
+    mobile_num = request.json.get('mobile_num')
+    if mobile_num is None or mobile_num == '':
+        raise ValidationError('user does not have a mobile_num')
+    password = request.json.get('password')
+    if password is None or password == '':
+        raise ValidationError('user does not have a password')
 
-        # verify code
-        code = request.json.get('code')
-        redis_key = ('YUNPHONE:VERIFYCODE:%s' % mobile_num).upper()
-        code_redis = redis_store.get(redis_key)
+    # verify code
+    code = request.json.get('code')
+    redis_key = ('YUNPHONE:VERIFYCODE:%s' % mobile_num).upper()
+    code_redis = redis_store.get(redis_key)
 
-        if not code_redis or code != code_redis:
-            return jsonify(BaseApi.api_wrong_verify_code())
+    if not code_redis or code != code_redis:
+        return jsonify(BaseApi.api_wrong_verify_code())
 
-        now_user = User.query.filter_by(mobile_num=mobile_num).first()
-        if not now_user:
-            raise ValidationError('this user does not exists')
-        now_user.password = password
-        db.session.add(now_user)
-        db.session.commit()
-        return jsonify(BaseApi.api_success(now_user.to_json()))
-    except BaseException, e:
-        db.session.rollback()
-        app.logger.error(e.message)
-        return jsonify(BaseApi.api_system_error(e.message))
+    now_user = User.query.filter_by(mobile_num=mobile_num).first()
+    if not now_user:
+        raise ValidationError('this user does not exists')
+    now_user.password = password
+    db.session.add(now_user)
+    db.session.commit()
+    return jsonify(BaseApi.api_success(now_user.to_json()))
+    # except BaseException, e:
+    #     db.session.rollback()
+    #     app.logger.error(e.message)
+    #     return jsonify(BaseApi.api_system_error(e.message))
