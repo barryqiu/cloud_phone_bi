8f05963280f1c105cd45d2334a7f0612e5ecb514
barryqiu <qiuxiaobing90@126.com>
2016-04-07 12:38:33 +0800

    database

--- app/api_1_1/users.py
+++ app/api_1_1/users.py
@@ -8,6 +8,6 @@ from flask import current_app as app
 def get_user():
     try:
         return jsonify(BaseApi.api_success(g.current_user.to_json()))
-    except Exception, e:
-        app.logger.error(e.message)
+    except Exception as e:
+        app.logger.exception('info')
         return jsonify(BaseApi.api_system_error(e.message))
