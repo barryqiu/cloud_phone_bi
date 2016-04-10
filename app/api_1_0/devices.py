46d308b7895c500a93e4a240205ab708693db447
barryqiu <qiuxiaobing90@126.com>
2016-04-10 14:34:51 +0800

    database

--- app/api_1_0/devices.py
+++ app/api_1_0/devices.py
@@ -61,7 +61,7 @@ def new_device():
         # add device to queue
         ret = Device.push_redis_set(device.id)
         f = open('newdevice.log', 'a')
-        f.write(("add device_id: %s to redis set return %s\n" % (device.id, ret)))
+        f.write(("%s:add device_name: %s, device_id: %s to redis set return %s\n" % (datetime, device.device_name, device.id, ret)))
         f.close()
 
         return jsonify(BaseApi.api_success(device.to_json()))
