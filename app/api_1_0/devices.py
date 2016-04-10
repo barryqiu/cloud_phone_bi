135251dc39ac05040036a3f41aa3a9e51648fb08
barryqiu <qiuxiaobing90@126.com>
2016-04-10 13:38:52 +0800

    database

--- app/api_1_0/devices.py
+++ app/api_1_0/devices.py
@@ -58,10 +58,9 @@ def new_device():
         db.session.add(device)
         db.session.commit()
 
-        print "new device id %s " % device.id
         # add device to queue
         ret = Device.push_redis_set(device.id)
-        print "add to redis set ret %s" % ret
+        print "add device_id: %s to redis set return %s" % device.id, ret
 
         return jsonify(BaseApi.api_success(device.to_json()))
     except Exception as e:
