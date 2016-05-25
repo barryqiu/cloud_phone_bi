41f67beb2e3aac836eef4191a95c3913d068034a
barryqiu <qiuxiaobing90@126.com>
2016-05-25 21:04:25 +0800

    jpush 超时机制

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 import random
+import threading
 import time
 import jpush
+import sys
 from flask import current_app as app
 from upyun import upyun
 
@@ -26,6 +30,96 @@ def generate_verification_code():
     return verification_code
 
 
+def filter_upload_url(url):
+    if not url:
+        return ''
+    if url.startswith('http:'):
+        return url
+    return app.config['UPLOAD_HOST'] + url
+
+
+class KThread(threading.Thread):
+    """A subclass of threading.Thread, with a kill()
+    method.
+
+    Come from:
+    Kill a thread in Python:
+    http://mail.python.org/pipermail/python-list/2004-May/260937.html
+    """
+
+    def __init__(self, *args, **kwargs):
+        threading.Thread.__init__(self, *args, **kwargs)
+        self.killed = False
+
+    def start(self):
+        """Start the thread."""
+        self.__run_backup = self.run
+        self.run = self.__run  # Force the Thread to install our trace.
+        threading.Thread.start(self)
+
+    def __run(self):
+        """Hacked run function, which installs the
+        trace."""
+        sys.settrace(self.globaltrace)
+        self.__run_backup()
+        self.run = self.__run_backup
+
+    def globaltrace(self, frame, why, arg):
+        if why == 'call':
+            return self.localtrace
+        else:
+            return None
+
+    def localtrace(self, frame, why, arg):
+        if self.killed:
+            if why == 'line':
+                raise SystemExit()
+        return self.localtrace
+
+    def kill(self):
+        self.killed = True
+
+
+class Timeout(Exception):
+    """function run timeout"""
+
+
+def timeout(seconds):
+    """超时装饰器，指定超时时间
+    若被装饰的方法在指定的时间内未返回，则抛出Timeout异常"""
+
+    def timeout_decorator(func):
+        """真正的装饰器"""
+
+        def _new_func(oldfunc, result, oldfunc_args, oldfunc_kwargs):
+            result.append(oldfunc(*oldfunc_args, **oldfunc_kwargs))
+
+        def _(*args, **kwargs):
+            result = []
+            new_kwargs = {  # create new args for _new_func, because we want to get the func return val to result list
+                'oldfunc': func,
+                'result': result,
+                'oldfunc_args': args,
+                'oldfunc_kwargs': kwargs
+            }
+            thd = KThread(target=_new_func, args=(), kwargs=new_kwargs)
+            thd.start()
+            thd.join(seconds)
+            alive = thd.isAlive()
+            thd.kill()  # kill the child thread
+            if alive:
+                raise Timeout(u'function run too long, timeout %d seconds.' % seconds)
+            else:
+                return result[0]
+
+        _.__name__ = func.__name__
+        _.__doc__ = func.__doc__
+        return _
+
+    return timeout_decorator
+
+
+@timeout(2)
 def push_message_to_alias(content, msg_type, alias, platform='android'):
     msg = {
         'msg_type': msg_type,
@@ -41,6 +135,7 @@ def push_message_to_alias(content, msg_type, alias, platform='android'):
     return ret.payload['sendno'].encode('utf-8')
 
 
+@timeout(2)
 def upload_to_cdn(path, file_path):
     try:
         up = upyun.UpYun(app.config['CDN_BUCKET'], username=app.config['CDN_USER_NAME'],
@@ -50,11 +145,3 @@ def upload_to_cdn(path, file_path):
         return app.config['CDN_HOST'] + path
     except Exception:
         return ''
-
-
-def filter_upload_url(url):
-    if not url:
-        return ''
-    if url.startswith('http:'):
-        return url
-    return app.config['UPLOAD_HOST'] + url
