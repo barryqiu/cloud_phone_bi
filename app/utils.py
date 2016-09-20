5dc3416c6740bede7fb2a8e97c899967d1650efd
barryqiu <qiuxiaobing90@126.com>
2016-09-20 20:43:59 +0800

    commit

--- app/utils.py
+++ app/utils.py
@@ -71,7 +71,7 @@ def push_message_to_device(device_name, content, msg_type):
 
         response = urllib2.urlopen(req, json.dumps(msg), timeout=2)
 
-        app.logger.error("%s:%s" % (json.dumps(msg), response.code))
+        app.logger.error("%s:%s:%s" % (device_name, json.dumps(msg), response.code))
         if response.code == 200:
             return True
     except Exception as e:
