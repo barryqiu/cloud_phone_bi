bcaa1c61bc8e5df2f076ac630dcc72ae0ed34e14
barryqiu <qiuxiaobing90@126.com>
2016-09-26 17:50:42 +0800

    commit

--- app/api_1_1/devices.py
+++ app/api_1_1/devices.py
@@ -328,7 +328,7 @@ def device_map(alias):
         device_name = Device.get_device_map(alias)
         if device_name:
             jsonify(BaseApi.api_success("success"))
-        raise ValidationError('alias %s not exist' % alias)
+        raise ValidationError('alias %s, ret %s' % (alias, device_name))
     except Exception as e:
         app.logger.exception('info')
         return jsonify(BaseApi.api_system_error(e.message))
