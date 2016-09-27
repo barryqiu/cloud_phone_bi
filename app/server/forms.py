31cbb29ea5d148a2c323a83401ddd47cb13e08c6
barryqiu <qiuxiaobing90@126.com>
2016-09-27 21:01:53 +0800

    commit

--- app/server/forms.py
+++ app/server/forms.py
@@ -1,4 +1,4 @@
-5# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, FileField, HiddenField, TextAreaField
 from wtforms.validators import Length, DataRequired
