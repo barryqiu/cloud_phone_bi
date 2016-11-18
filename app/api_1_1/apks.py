dbf7820997f0aa92281f24eff43d5565f0ad179d
barryqiu <qiuxiaobing90@126.com>
2016-11-18 09:45:28 +0800

    commit

--- app/api_1_1/apks.py
+++ app/api_1_1/apks.py
@@ -1,4 +1,6 @@
 from flask import jsonify
+
+from ..utils import convert_pagination
 from . import api1_1
 from ..api_1_0.base_api import BaseApi
 from ..models import *
@@ -13,3 +15,11 @@ def get_rec_apk():
     except Exception as e:
         app.logger.exception('info')
         return jsonify(BaseApi.api_system_error(e.message))
+
+
+@api1_1.route('/apk/<int:page>')
+def get_apk(page):
+    pagination = Apk.query.order_by(Apk.add_time.desc()).paginate(
+        page, per_page=app.config['DEVICE_NUM_PER_PAGE'], error_out=False)
+    ret = {'apks': [apk.to_json() for apk in pagination.items], 'pageinfo': convert_pagination(pagination)}
+    return jsonify(BaseApi.api_success(ret))
