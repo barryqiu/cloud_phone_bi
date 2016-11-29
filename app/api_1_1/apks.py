7cdbba99a655d42b6e1201025b5658f9809db3f7
barryqiu <qiuxiaobing90@126.com>
2016-11-29 12:58:06 +0800

    commit

--- app/api_1_1/apks.py
+++ app/api_1_1/apks.py
@@ -19,7 +19,33 @@ def get_rec_apk():
 
 @api1_1.route('/apk/<int:page>')
 def get_apk(page):
-    pagination = Apk.query.order_by(Apk.add_time.desc()).paginate(
-        page, per_page=app.config['DEVICE_NUM_PER_PAGE'], error_out=False)
-    ret = {'apks': [apk.to_json() for apk in pagination.items], 'pageinfo': convert_pagination(pagination)}
-    return jsonify(BaseApi.api_success(ret))
+    try:
+        pagination = Apk.query.order_by(Apk.add_time.desc()).paginate(page, per_page=app.config['DEVICE_NUM_PER_PAGE'],
+                                                                      error_out=False)
+        ret = {'apks': [apk.to_json() for apk in pagination.items], 'pageinfo': convert_pagination(pagination)}
+        return jsonify(BaseApi.api_success(ret))
+    except Exception as e:
+        app.logger.exception('info')
+        return jsonify(BaseApi.api_system_error(e.message))
+
+
+@api1_1.route('/category')
+def get_apk_category():
+    try:
+        category_list = Category.query.filter_by(state=1).all()
+        return jsonify(BaseApi.api_success([category.to_json() for category in category_list]))
+    except Exception as e:
+        app.logger.exception('info')
+        return jsonify(BaseApi.api_system_error(e.message))
+
+
+@api1_1.route('/category/<int:category_id>/apk/<int:page>')
+def get_apk_by_category(category_id, page):
+    try:
+        pagination = CategoryApk.query.filter_by(category_id=category_id).paginate(
+            page, per_page=app.config['GAME_NUM_PER_PAGE'], error_out=False)
+        ret = {'apks': [category_apk.apk.to_json() for category_apk in pagination.items], 'pageinfo': convert_pagination(pagination)}
+        return jsonify(BaseApi.api_success(ret))
+    except Exception as e:
+        app.logger.exception('info')
+        return jsonify(BaseApi.api_system_error(e.message))
